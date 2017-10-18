1aaaa9a0d67b598f76d2c00555f7b9b8c689bd41
Matt Chinnock <mattchinnock@gmail.com>
2017-10-18 13:57:47 -1000

    Handle imports of icons

--- app/assets/icons/icons.js
+++ app/assets/icons/icons.js
@@ -1,2 +1,25 @@
 import Wind from './wind.svg';
-export { Wind as Wind };
+import Rain from './rain.svg';
+import Cloud from './cloud.svg';
+import Sun from './sun.svg';
+import Moon from './moon.svg';
+import Snow from './snow.svg';
+import Sleet from './sleet.svg';
+import Fog from './fog.svg';
+import CloudSun from './partly-cloudy-day.svg';
+import CloudMoon from './partly-cloudy-night.svg';
+
+export const iconTypes = {
+  'wind' : Wind,
+  'rain' : Rain,
+  'cloudy' : Cloud,
+  'clear-day' : Sun,
+  'clear-night' : Moon,
+  'snow' : Snow,
+  'sleet' : Sleet,
+  'fog' : Fog,
+  'partly-cloudy-day' : CloudSun,
+  'partly-cloudy-night' : CloudMoon
+}
+
+export { Wind, Rain, Cloud, Sun, Moon, Snow, Sleet, Fog, CloudSun, CloudMoon };
